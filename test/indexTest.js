c82b2281aa7a809be4e262546b69313c67ef8fb5
Kellye Greene <kellyecodes@gmail.com>
2019-04-24 16:00:39 +0200

    Adds missing const

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,10 @@
+const fs = require('file-system')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+const babel = require('babel-core');
+
+const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+
 describe('index.js', function () {
   describe('companyName', function () {
     it('is set as Scuber', function () {
