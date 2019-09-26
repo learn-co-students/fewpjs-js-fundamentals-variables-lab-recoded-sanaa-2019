e0ee49ed5f4a321001713842b36373ed0753e7ec
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-09-25 21:35:39 -0700

    add index.js info

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+mostProfitableNeighborhood = "Upper West Side";
+let companyCeo = "Susan Smith";
+companyCeo = "Lauren Hart";
