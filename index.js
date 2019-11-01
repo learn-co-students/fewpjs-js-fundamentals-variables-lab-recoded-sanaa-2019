d280293cba736d6ca2339b5fdcc25a2ac746834b
TaghreedNageeb <taghreed.n.d2018@gmail.com>
2019-11-01 07:46:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Upper West Side';
+console.log(mostProfitableNeighborhood);
+mostProfitableNeighborhood = 'Chelsea';
+let companyCeo= 'Lauren Hart';
+companyCeo = 'Susan Smith';
+
+console.log(mostProfitableNeighborhood)
